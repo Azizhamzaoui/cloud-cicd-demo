738891e970577f7bd7471b1202a9cd49e787c0bb
Azizhamzaoui <hamzaoui.mohamedaziz@esprit.tn>
2026-05-04 11:45:07 +0100

    testing after modifying test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import sys
+import os
 
-sys.path.append("/home/runner/work/cloud-cicd-demo/cloud-cicd-demo")
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app
 
